50e4c51a1383b28458f7aea124ace585b692cc30
abochev <aleksandr.bochev@zalando.de>
2025-01-22 10:06:31 +0100

    update the extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Layout Switch Shortcut - The shortcut to switch the layout of the selected text */
+  "layoutSwitchModifier": "command" | "control" | "option",
+  /** Russian Layout - The name of the Russian layout */
+  "ruLayoutName": string,
+  /** English Layout - The name of the English layout */
+  "enLayoutName": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `index` command */
+  export type Index = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `index` command */
+  export type Index = {}
+}
+
